e614448c29662ca970d8ea6f9cf297a4429776f1
Miura55 <acordion.piano@gmail.com>
2020-11-27 01:19:34 +0900

    Add base node of land

--- node.js
+++ node.js
@@ -1,17 +1,21 @@
 module.exports = function (RED) {
-  function LeaveNode(config) {
+  function takeoffNode(config) {
     RED.nodes.createNode(this, config);
-    this.inputText = config.inputText;
     var node = this;
     node.on("input", function (msg) {
       msg.payload = msg.payload.toLowerCase();
-      // logging
-      var log4js = require("log4js");
-      var log = log4js.getLogger();
-      log.level = "debug";
-      log.debug(node.inputText);
       node.send(msg);
     });
   }
-  RED.nodes.registerType("Leave", LeaveNode);
+  RED.nodes.registerType("takeoff", takeoffNode);
+
+  function landNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on("input", function (msg) {
+      msg.payload = msg.payload.toLowerCase();
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("land", landNode);
 };
