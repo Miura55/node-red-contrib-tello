a4f5887f9b5c3abc92ead07bfa4b7687a2b6ddf0
Miura55 <acordion.piano@gmail.com>
2020-11-28 01:40:25 +0900

    Change file name

--- tello.js
+++ tello.js
@@ -1,9 +1,25 @@
+const dgram = require("dgram");
+const client = dgram.createSocket("udp4");
+
+const HOST = "192.168.10.1";
+const PORT = 8889;
+
+const sendCommand = (command) => {
+  const message = Buffer.from(command);
+  client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
+    if (err) throw err;
+  });
+};
+
+// Ready to Flight
+sendCommand("command");
+
 module.exports = function (RED) {
   function takeoffNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on("input", function (msg) {
-      msg.payload = msg.payload.toLowerCase();
+      sendCommand("takeoff");
       node.send(msg);
     });
   }
@@ -13,7 +29,7 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on("input", function (msg) {
-      msg.payload = msg.payload.toLowerCase();
+      sendCommand("land");
       node.send(msg);
     });
   }
